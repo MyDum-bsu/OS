82ebfa471b8a10a6bde7670960c3aace48bb2449
MyDum-bsu <mydum.bsu@gmail.com>
2022-10-12 22:16:35 +0300

    Gates of Babylon

--- linux/lab2/main.cpp
+++ linux/lab2/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <random>
 #include <chrono>
+#include <vector>
 #include <thread>
+#include <algorithm>
 
 std::vector<int> generateMatrix(int n) {
     std::vector<int> matrix(n * n);
     std::mt19937 rnd(std::chrono::system_clock::now().time_since_epoch().count());
     for (int i = 0; i < n * n; i++) {
-        matrix[i] = (int) rnd() % 2;
+        matrix[i] = (int) (rnd()) % 100;
     }
     return matrix;
 }
@@ -47,10 +48,51 @@ std::vector<int> multiplyMatrixWithThread(const std::vector<int> &A, const std::
     for (std::thread &thread: threads) {
         thread.join();
     }
+    return C;
+}
+
+std::vector<long> generateTimeDependence(const std::vector<int> &matrixA, const std::vector<int> &matrixB, int n) {
+    std::vector<long> times(2 * n);
+
+    for (int i = 1; i < n; i++) {
+        auto start = std::chrono::system_clock::now();
+        multiplyMatrixSequentially(matrixA, matrixB, i);
+        auto end = std::chrono::system_clock::now();
+
+        auto deltaSequentially = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        start = std::chrono::system_clock::now();
+        multiplyMatrixWithThread(matrixA, matrixB, i);
+        end = std::chrono::system_clock::now();
+
+        auto deltaWithThread = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        times[i] = deltaSequentially;
+        times[n + i] = deltaWithThread;
+    }
+    return times;
+}
+
+void run(int n) {
+    std::vector<int> A = generateMatrix(n);
+    std::vector<int> B = generateMatrix(n);
+    std::vector<long> timeDependence = generateTimeDependence(A, B, n);
+    double efficient = 0;
+    int blockSizeOfMostEffective = 0;
+    for (int i = 0; i < n - 1; i++) {
+        double temp = 1. * timeDependence[i + 1] / timeDependence[n + i + 1];
+        if (temp > efficient) {
+            blockSizeOfMostEffective = i+1;
+            efficient = temp;
+        }
+        std::cout << "Block size: " << i + 1 << " Amount of blocks: " << ceil((n * 1. / (i + 1))) * n
+                  << " Delta for sequential: " << timeDependence[i + 1]
+                  << " Delta for thread: " << timeDependence[n + i + 1] << " Efficient: "
+                  << 1. * timeDependence[i + 1] / timeDependence[n + i + 1] << std::endl;
+    }
+    std::cout << "\nBlock size with most effective multithreading: " << blockSizeOfMostEffective << " Speed up: " << efficient;
 }
 
 int main() {
-    std::vector<int> matrix = generateMatrix(3);
-    std::vector<int> mult = multiplyMatrixSequentially(matrix, matrix, 2);
-    return 0;
+    int n = 100;
+    run(n);
 }
