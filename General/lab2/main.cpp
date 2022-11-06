1ce02bdc3506227b198f4a182fec4f424bfa499a
MyDum-bsu <mydum.bsu@gmail.com>
2022-11-06 23:34:45 +0300

    Strangeways

---
+++ General/lab2/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <thread>
+#include <algorithm>
+
+std::vector<int> generateMatrix(int n) {
+    std::vector<int> matrix(n * n);
+    std::mt19937 rnd(std::chrono::system_clock::now().time_since_epoch().count());
+    for (int i = 0; i < n * n; i++) {
+        matrix[i] = (int) (rnd()) % 100;
+    }
+    return matrix;
+}
+
+void multiplyBlocks(const std::vector<int> &A, const std::vector<int> &B, std::vector<int> &C, int n, int blockSize,
+                    int upperLeftI, int upperLeftJ) {
+    for (int i = upperLeftI; i < std::min(upperLeftI + blockSize, n); i++) {
+        for (int j = upperLeftJ; j < std::min(upperLeftJ + blockSize, n); j++) {
+            for (int k = 0; k < n; k++) {
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
+            }
+        }
+    }
+}
+
+std::vector<int> multiplyMatrixSequentially(const std::vector<int> &A, const std::vector<int> &B, int blockSize) {
+    int matrixSize = (int) std::sqrt(A.size());
+    std::vector<int> C(matrixSize * matrixSize, 0);
+    for (int i = 0; i < matrixSize; i += blockSize) {
+        for (int j = 0; j < matrixSize; j += blockSize) {
+            multiplyBlocks(A, B, C, matrixSize, blockSize, i, j);
+        }
+    }
+    return C;
+}
+
+std::vector<int> multiplyMatrixWithThread(const std::vector<int> &A, const std::vector<int> &B, int blockSize) {
+    int matrixSize = (int) std::sqrt(A.size());
+    std::vector<int> C(matrixSize * matrixSize, 0);
+    std::vector<std::thread> threads;
+    for (int i = 0; i < matrixSize; i += blockSize) {
+        for (int j = 0; j < matrixSize; j += blockSize) {
+            threads.emplace_back(multiplyBlocks, std::cref(A), std::cref(B), std::ref(C), matrixSize, blockSize, i, j);
+        }
+    }
+    for (std::thread &thread: threads) {
+        thread.join();
+    }
+    return C;
+}
+
+std::vector<long> generateTimeDependence(const std::vector<int> &matrixA, const std::vector<int> &matrixB, int n) {
+    std::vector<long> times(2 * n);
+
+    for (int i = 1; i < n; i++) {
+        auto start = std::chrono::system_clock::now();
+        multiplyMatrixSequentially(matrixA, matrixB, i);
+        auto end = std::chrono::system_clock::now();
+
+        auto deltaSequentially = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        start = std::chrono::system_clock::now();
+        multiplyMatrixWithThread(matrixA, matrixB, i);
+        end = std::chrono::system_clock::now();
+
+        auto deltaWithThread = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        times[i] = deltaSequentially;
+        times[n + i] = deltaWithThread;
+    }
+    return times;
+}
+
+void run(int n) {
+    std::vector<int> A = generateMatrix(n);
+    std::vector<int> B = generateMatrix(n);
+    std::vector<long> timeDependence = generateTimeDependence(A, B, n);
+    double efficient = 0;
+    int blockSizeOfMostEffective = 0;
+    for (int i = 0; i < n - 1; i++) {
+        double temp = 1. * timeDependence[i + 1] / timeDependence[n + i + 1];
+        if (temp > efficient) {
+            blockSizeOfMostEffective = i+1;
+            efficient = temp;
+        }
+        std::cout << "Block size: " << i + 1 << " Amount of blocks: " << ceil((n * 1. / (i + 1))) * n
+                  << " Delta for sequential: " << timeDependence[i + 1]
+                  << " Delta for thread: " << timeDependence[n + i + 1] << " Efficient: "
+                  << 1. * timeDependence[i + 1] / timeDependence[n + i + 1] << std::endl;
+    }
+    std::cout << "\nBlock size with most effective multithreading: " << blockSizeOfMostEffective << " Speed up: " << efficient;
+}
+
+int main() {
+    int n = 100;
+    run(n);
+}
--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 
 [Lab 1 for linux](https://github.com/MyDum-bsu/OS/tree/main/linux/lab1) - аналогично для Linux
 
-[Lab 2](https://github.com/MyDum-bsu/OS/tree/main/linux/lab2) - matrix multiplication for all platforms
+[Lab 2](https://github.com/MyDum-bsu/OS/tree/main/General/lab2) - matrix multiplication for all platforms
 
 [Lab 2](https://github.com/MyDum-bsu/OS/tree/main/windows/lab2) - matrix multiplication for windows
 
-[Lab 3](https://github.com/MyDum-bsu/OS/tree/main/linux/lab3) - buffered channel
+[Lab 3](https://github.com/MyDum-bsu/OS/tree/main/General/lab3) - buffered channel
 
-[Lab 4](https://github.com/MyDum-bsu/OS/tree/main/linux/lab4) - Lab2 + Lab3
+[Lab 4](https://github.com/MyDum-bsu/OS/tree/main/General/lab4) - Lab2 + Lab3
